2452fbe05c0e30aa425cfe7670ea2b6f7421d5f2
Grzegorz Beń <grzegorz.ben@coderush.pl>
2020-06-19 00:23:13 +0200

    fix converter on null value

--- src/converters.ts
+++ src/converters.ts
@@ -2,7 +2,12 @@ import { DatetimeValueConverter, SetValueConverter } from '@spinajs/orm';
 
 export class SqliteDatetimeValueConverter extends DatetimeValueConverter {
   public toDB(value: Date) {
-    return value.toISOString();
+    if(value instanceof Date)
+    {
+      return value.toISOString();
+    }
+
+    return null;
   }
 
   public fromDB(value: string) {
