1146d5e20bc1b829297f6cb119e78b94a599064d
Grzegorz Beń <grzegorz.ben@coderush.pl>
2021-09-07 11:54:18 +0200

    date to iso string

--- src/converters.ts
+++ src/converters.ts
@@ -3,17 +3,34 @@ import { DatetimeValueConverter } from '@spinajs/orm';
 export class SqliteDatetimeValueConverter extends DatetimeValueConverter {
   public toDB(value: Date) {
     if (value instanceof Date) {
-      return value.toISOString();
+      return this.toIsoString(value);
     }
 
     return null;
   }
 
   public fromDB(value: string) {
-    if(!value){
+    if (!value) {
       return null;
     }
-    
+
     return new Date(value);
+  }
+
+  public toIsoString(date: Date) {
+    const tzo = -date.getTimezoneOffset();
+    const dif = tzo >= 0 ? '+' : '-';
+    const pad = (num: number) => {
+      const norm = Math.floor(Math.abs(num));
+      return (norm < 10 ? '0' : '') + norm;
+    };
+    return date.getFullYear() +
+      '-' + pad(date.getMonth() + 1) +
+      '-' + pad(date.getDate()) +
+      'T' + pad(date.getHours()) +
+      ':' + pad(date.getMinutes()) +
+      ':' + pad(date.getSeconds()) +
+      dif + pad(tzo / 60) +
+      ':' + pad(tzo % 60);
   }
 }
